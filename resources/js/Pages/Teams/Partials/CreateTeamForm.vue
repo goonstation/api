e6eec764c0cb52f61192287f52949efe54aaecbc
Wire <wirewraith@gmail.com>
2023-06-30 10:28:37 -0400

    Mostly complete admin preferences UI

--- resources/js/Pages/Teams/Partials/CreateTeamForm.vue
+++ resources/js/Pages/Teams/Partials/CreateTeamForm.vue
@@ -1,66 +1,71 @@
 <script setup>
-import { useForm } from '@inertiajs/vue3';
-import FormSection from '@/Components/Jetstream/FormSection.vue';
-import InputError from '@/Components/Jetstream/InputError.vue';
-import InputLabel from '@/Components/Jetstream/InputLabel.vue';
-import PrimaryButton from '@/Components/Jetstream/PrimaryButton.vue';
-import TextInput from '@/Components/Jetstream/TextInput.vue';
+import { useForm } from '@inertiajs/vue3'
 
 const form = useForm({
-    name: '',
-});
+  name: '',
+})
 
 const createTeam = () => {
-    form.post(route('teams.store'), {
-        errorBag: 'createTeam',
-        preserveScroll: true,
-    });
-};
+  form.post(route('teams.store'), {
+    errorBag: 'createTeam',
+    preserveScroll: true,
+  })
+}
 </script>
 
 <template>
-    <FormSection @submitted="createTeam">
-        <template #title>
-            Team Details
-        </template>
+  <div class="row q-col-gutter-sm">
+    <div class="col-12 col-md-4">
+      <h3 class="q-mb-sm text-h6">Team Details</h3>
+      <div class="q-mb-md">Create a new team to collaborate with others on projects.</div>
+    </div>
+    <div class="col-12 col-md-8">
+      <q-card flat class="q-pa-sm">
+        <q-card-section>
+          <q-form @submit="createTeam">
+            <div class="q-mb-md text-weight-medium text-body1">Team Owner</div>
 
-        <template #description>
-            Create a new team to collaborate with others on projects.
-        </template>
+            <div class="flex items-center q-mt-sm q-mb-md">
+              <img
+                style="width: 50px; border-radius: 50%"
+                :src="$page.props.user.profile_photo_url"
+                :alt="$page.props.user.name"
+              />
 
-        <template #form>
-            <div class="col-span-6">
-                <InputLabel value="Team Owner" />
-
-                <div class="flex items-center mt-2">
-                    <img class="object-cover w-12 h-12 rounded-full" :src="$page.props.user.profile_photo_url" :alt="$page.props.user.name">
-
-                    <div class="ml-4 leading-tight">
-                        <div>{{ $page.props.user.name }}</div>
-                        <div class="text-sm text-gray-700">
-                            {{ $page.props.user.email }}
-                        </div>
-                    </div>
+              <div class="q-ml-md">
+                <div>{{ $page.props.user.name }}</div>
+                <div class="text-grey-6 text-sm">
+                  {{ $page.props.user.email }}
                 </div>
+              </div>
             </div>
 
-            <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="name" value="Team Name" />
-                <TextInput
-                    id="name"
-                    v-model="form.name"
-                    type="text"
-                    class="block w-full mt-1"
-                    autofocus
-                />
-                <InputError :message="form.errors.name" class="mt-2" />
-            </div>
-        </template>
+            <q-input
+              v-model="form.name"
+              class="q-mb-sm"
+              type="text"
+              label="Team Name"
+              filled
+              required
+              hide-bottom-space
+              autofocus
+              :error="!!form.errors.name"
+              :error-message="form.errors.name"
+            />
 
-        <template #actions>
-            <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Create
-            </PrimaryButton>
-        </template>
-    </FormSection>
+            <div class="flex items-center q-mt-md">
+              <q-space />
+              <q-btn
+                label="Create"
+                type="submit"
+                color="primary"
+                text-color="black"
+                :loading="form.processing"
+              />
+            </div>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
 </template>
--- resources/js/Pages/Teams/Show.vue
+++ resources/js/Pages/Teams/Show.vue
@@ -1,42 +1,28 @@
 <script setup>
-import AdminLayout from '@/Layouts/AdminLayout.vue';
-import DeleteTeamForm from './Partials/DeleteTeamForm.vue';
-import SectionBorder from '@/Components/Jetstream/SectionBorder.vue';
-import TeamMemberManager from './Partials/TeamMemberManager.vue';
-import UpdateTeamNameForm from './Partials/UpdateTeamNameForm.vue';
+import AdminLayout from '@/Layouts/AdminLayout.vue'
+import DeleteTeamForm from './Partials/DeleteTeamForm.vue'
+import TeamMemberManager from './Partials/TeamMemberManager.vue'
+import UpdateTeamNameForm from './Partials/UpdateTeamNameForm.vue'
 
 defineProps({
-    team: Object,
-    availableRoles: Array,
-    permissions: Object,
-});
+  team: Object,
+  availableRoles: Array,
+  permissions: Object,
+})
 </script>
 
 <template>
-    <AdminLayout title="Team Settings">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Team Settings
-            </h2>
-        </template>
+  <AdminLayout title="Team Settings">
+    <UpdateTeamNameForm :team="team" :permissions="permissions" />
 
-        <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                <UpdateTeamNameForm :team="team" :permissions="permissions" />
+    <TeamMemberManager
+      :team="team"
+      :available-roles="availableRoles"
+      :user-permissions="permissions"
+    />
 
-                <TeamMemberManager
-                    class="mt-10 sm:mt-0"
-                    :team="team"
-                    :available-roles="availableRoles"
-                    :user-permissions="permissions"
-                />
-
-                <template v-if="permissions.canDeleteTeam && ! team.personal_team">
-                    <SectionBorder />
-
-                    <DeleteTeamForm class="mt-10 sm:mt-0" :team="team" />
-                </template>
-            </div>
-        </div>
-    </AdminLayout>
+    <template v-if="permissions.canDeleteTeam && !team.personal_team">
+      <DeleteTeamForm class="q-mt-lg" :team="team" />
+    </template>
+  </AdminLayout>
 </template>
