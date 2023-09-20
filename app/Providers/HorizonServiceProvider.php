4ada11024e17435adb146fc0aeabe18cc18e74d5
Wire <wirewraith@gmail.com>
2023-09-20 14:42:04 -0400

    Guards for horizon and telescope

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
@@ -27,8 +28,8 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      */
     protected function gate(): void
     {
-        Gate::define('viewHorizon', function ($user = null) {
-            return true;
+        Gate::define('viewHorizon', function (User $user) {
+            return $user->is_admin;
         });
     }
 }
