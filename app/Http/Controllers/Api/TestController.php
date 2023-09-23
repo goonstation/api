26187655cfb5932dc9e717b2adde965ae0cea3f4
Wire <wirewraith@gmail.com>
2023-09-23 11:48:24 -0400

    Doc for the dumb test route

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
+    /**
+     * Test
+     */
     public function index(Request $request)
     {
         return response()->json([
