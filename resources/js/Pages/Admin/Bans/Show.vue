d6e4c39184744336e91c75c75f6f1a50e24b0782
Wire <wirewraith@gmail.com>
2024-01-31 11:37:47 -0500

    Delete ban from individual view

--- resources/js/Pages/Admin/Bans/Show.vue
+++ resources/js/Pages/Admin/Bans/Show.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="text-right q-mb-md">
+  <div v-if="!isDeleted" class="text-right q-mb-md">
     <Link :href="route('admin.bans.edit', ban.id)">
-      <q-btn color="primary" outline>
-        Edit Ban
-      </q-btn>
+      <q-btn color="primary" outline> Edit Ban </q-btn>
     </Link>
+    <q-btn @click="openConfirmDelete" color="negative" class="q-ml-md" outline>
+      Delete Ban
+    </q-btn>
   </div>
 
   <div class="row q-col-gutter-md">
@@ -115,6 +116,20 @@
     <div class="col-12 col-md-6">
       <ban-details :model-value="ban" />
     </div>
+
+    <q-dialog v-model="confirmDelete">
+      <q-card flat bordered>
+        <q-card-section class="row items-center no-wrap">
+          <q-avatar :icon="ionInformationCircleOutline" color="negative" text-color="dark" />
+          <span class="q-ml-sm"> Are you sure you want to delete this ban? </span>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn flat label="Confirm" color="negative" @click="deleteBan" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
@@ -128,14 +143,16 @@ tbody {
 
 <script>
 import dayjs from 'dayjs'
+import axios from 'axios'
 import { date } from 'quasar'
 import { router } from '@inertiajs/vue3'
+import { ionInformationCircleOutline } from '@quasar/extras/ionicons-v6'
 import AdminLayout from '@/Layouts/AdminLayout.vue'
 import BanDetails from './Partials/BanDetails.vue'
 
 export default {
   components: {
-    BanDetails
+    BanDetails,
   },
 
   layout: (h, page) =>
@@ -151,6 +168,13 @@ export default {
     return {
       dayjs,
       router,
+      ionInformationCircleOutline,
+    }
+  },
+
+  data() {
+    return {
+      confirmDelete: false,
     }
   },
 
@@ -178,6 +202,30 @@ export default {
       if (!this.ban.deleted_at) return null
       const deletedAt = new Date(this.ban.deleted_at)
       return date.formatDate(deletedAt, 'YYYY/MM/DD HH:mm')
+    },
+  },
+
+  methods: {
+    openConfirmDelete() {
+      this.confirmDelete = true
+    },
+
+    async deleteBan() {
+      try {
+        const response = await axios.delete(route('admin.bans.delete', { ban: this.ban.id }))
+        this.$q.notify({
+          message: response.data.message || 'Item successfully deleted.',
+          color: 'positive',
+        })
+        router.reload()
+      } catch {
+        this.$q.notify({
+          message: 'Failed to delete ban, please try again.',
+          color: 'negative',
+        })
+      }
+
+      this.confirmDelete = false
     },
   },
 }
