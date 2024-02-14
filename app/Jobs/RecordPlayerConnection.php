029b7c8112e8f210486a8fdb76845cf67da0d36e
Wire <wirewraith@gmail.com>
2024-02-14 12:20:45 -0500

    Ignore TypeErrors from GeoLite on player connection recording

--- app/Jobs/RecordPlayerConnection.php
+++ app/Jobs/RecordPlayerConnection.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use TypeError;
 
 class RecordPlayerConnection implements ShouldQueue
 {
@@ -40,7 +41,7 @@ class RecordPlayerConnection implements ShouldQueue
         $geoRecord = null;
         try {
             $geoRecord = $geoReader->country($this->data['ip']);
-        } catch (\Exception $e) {
+        } catch (\Exception|TypeError $e) {
             // pass
         }
 
