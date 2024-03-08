1b8b1322e97b6daac5fb117d4db9de0e86004c22
Wire <wirewraith@gmail.com>
2024-03-08 13:01:55 -0500

    First draft of making player saves work with just ckey input

---
+++ app/Rules/PlayerIdWithCkey.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Player;
+use Illuminate\Contracts\Validation\DataAwareRule;
+use Illuminate\Contracts\Validation\InvokableRule;
+
+class PlayerIdWithCkey implements InvokableRule, DataAwareRule
+{
+    /**
+     * All of the data under validation.
+     *
+     * @var array
+     */
+    protected $data = [];
+
+    /**
+     * Set the data under validation.
+     *
+     * @param  array  $data
+     * @return $this
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+ 
+        return $this;
+    }
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @return void
+     */
+    public function __invoke($attribute, $value, $fail)
+    {
+        if ((!array_key_exists('ckey', $this->data) || !$this->data['ckey']) && !$value) {
+            return $fail('The :attribute is required when ckey is empty');
+        }
+
+        if ($value !== '0') {
+            $playerExists = Player::where('id', $value)->exists();
+            if (!$playerExists) {
+                return $fail('The selected player ID is invalid');
+            }
+        }
+    }
+}
