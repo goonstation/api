820107e5a5b65a9e8e579900abc7b5ff98aa6c33
Wire <wirewraith@gmail.com>
2024-03-13 17:16:37 -0400

    Add log events

---
+++ app/Models/Events/EventLog.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Events;
+
+use App\Models\GameRound;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EventLog extends Model
+{
+    use HasFactory;
+
+    protected $table = 'events_logs';
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function gameRound()
+    {
+        return $this->belongsTo(GameRound::class, 'round_id');
+    }
+}
