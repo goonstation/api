e19d05348a7bcfe5d8c07a47db146ceeb3ac7998
Wire <wirewraith@gmail.com>
2024-04-10 10:16:52 -0400

    Events errors line column to int

---
+++ database/migrations/2024_04_10_141156_update_events_errors_line_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE events_errors ALTER COLUMN
+            line TYPE integer USING (line)::integer');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events_errors', function (Blueprint $table) {
+            $table->text('line')->change();
+        });
+    }
+};
