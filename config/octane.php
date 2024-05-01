b5d6bf4cf9f7a52e511d2266d03a80fae0d76722
Wire <wirewraith@gmail.com>
2024-05-01 17:21:09 -0400

    Swoole options

--- config/octane.php
+++ config/octane.php
@@ -220,7 +220,13 @@ return [
 
     'swoole' => [
         'options' => [
-            'package_max_length' => 1024 * 1024 * 100, // 100mb
+            'http_compression' => true,
+            'http_compression_level' => 6, // 1 - 9
+            'compression_min_length' => 20,
+            'package_max_length' => 1024 * 1024 * 100, // 100MB
+            'open_http2_protocol' => true,
+            'document_root' => public_path(),
+            'enable_static_handler' => true,
         ],
     ],
 ];
