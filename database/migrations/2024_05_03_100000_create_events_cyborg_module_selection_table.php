fe25c48bd90cba753609bdf0562004ebb307918f
Tarmunora <48066662+Tarmunora@users.noreply.github.com>
2024-05-03 15:56:34 -0500

    add migration for cyborg module selection event

---
+++ database/migrations/2024_05_03_100000_create_events_cyborg_module_selection_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events_cyborg_module_selection', function (Blueprint $table) {
+            $table->id();
+            $table->integer('round_id');
+
+            $table->text('player_id')->nullable();
+            $table->text('module')->nullable();
+            $table->text('borg_type')->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('round_id')->references('id')->on('game_rounds');
+            $table->foreign('player_id')->references('id')->on('players');
+
+            $table->index('round_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events_cyborg_module_selection');
+    }
+};
