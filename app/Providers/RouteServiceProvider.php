c490b885f067efcb37072bd7e673296251c1f24b
Wire <wirewraith@gmail.com>
2024-06-21 15:49:27 -0400

    No more api path

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,10 +34,6 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
-            Route::middleware('api')
                 ->domain('api.'.preg_replace('(^https?://)', '', config('app.url')))
                 ->group(base_path('routes/api.php'));
 
