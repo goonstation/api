f5faae6f5975f78f94696a93ef5b34e093c591e5
Wire <wirewraith@gmail.com>
2024-11-24 04:59:51 +0000

    upgrades or whatever

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+set -e
+
+git fetch
+git reset --hard "@{u}"
+git clean -fd
+composer install --classmap-authoritative --no-interaction --no-ansi --no-dev
+composer clear-cache
+bun install --frozen-lockfile
+bun run build
+php artisan optimize:clear
+php artisan event:cache
+php artisan config:cache
+php artisan route:clear
+php artisan breadcrumbs:cache
+php artisan route:cache
+php artisan migrate --force
