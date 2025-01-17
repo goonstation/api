4b6e3b519ff1c87485d6d964e7985953c3cb1dfa
Wire <wirewraith@gmail.com>
2025-01-17 11:33:51 -0500

    SEO stuff

---
+++ app/Console/Commands/GenerateSiteMap.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\Sitemap;
+
+class GenerateSiteMap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gh:generate-sitemap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate a sitemap.xml file';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Sitemap::create()
+            ->add([
+                '/',
+                '/changelog',
+                '/players',
+                '/rounds',
+                '/events',
+                '/events/stats',
+                '/events/deaths',
+                '/events/tickets',
+                '/events/fines',
+                '/events/antags',
+                '/events/errors',
+                '/maps',
+                '/medals',
+            ])
+            ->writeToFile(public_path('sitemap.xml'));
+
+        return Command::SUCCESS;
+    }
+}
