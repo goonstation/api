e7bbf8a5765ab41ef1a339be55cfc6afbb673f20
Wire <wirewraith@gmail.com>
2025-03-01 18:41:35 +0000

    Different method of handling bots for sessions

--- app/Http/Middleware/NoSessionForBots.php
+++ app/Http/Middleware/NoSessionForBots.php
@@ -3,16 +3,20 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Config;
 use Crawler;
 use Illuminate\Http\Request;
+use Illuminate\Session\SessionManager;
 
 class NoSessionForBots
 {
+    public function __construct(
+        private readonly SessionManager $sessionManager,
+    ) {}
+
     public function handle(Request $request, Closure $next)
     {
-        if (Crawler::isCrawler()) {
-            Config::set('session.driver', 'array');
+        if (Crawler::isCrawler($request->userAgent())) {
+            $this->sessionManager->getContainer()->make('config')->set('session.driver', 'array');
         }
 
         return $next($request);
