00b77576c3b5ac259de313cd45a4e96381b4a5bf
Wire <wirewraith@gmail.com>
2025-03-02 14:48:21 +0000

    begone sentry on dev

--- app/Exceptions/Sentry.php
+++ app/Exceptions/Sentry.php
@@ -8,13 +8,15 @@ class Sentry
 {
     public static function tracesSampler(SamplingContext $context): float
     {
-        if ($context->getParentSampled()) {
+        $sampleRate = config('sentry.traces_sample_rate', null);
+
+        if ($sampleRate && $context->getParentSampled()) {
             // If the parent transaction (for example a JavaScript front-end)
             // is sampled, also sample the current transaction
             return 1.0;
         }
 
         // Default sample rate for all other transactions (replaces `traces_sample_rate`)
-        return config('sentry.traces_sample_rate', null) === null ? 0 : config('sentry.traces_sample_rate');
+        return $sampleRate === null ? 0.0 : $sampleRate;
     }
 }
